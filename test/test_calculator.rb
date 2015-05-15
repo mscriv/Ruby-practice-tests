0cdb035eea5514c5f0c50a85324a73ba7f66dc87
Michael Scriven <bigscriv03@gmail.com>
2015-05-15 07:09:56 -0400

    Dryed code

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -2,21 +2,21 @@ require 'test/unit'
 require 'app/calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  def setup
+    @calculator = Calculator.new
+  end
   def test_can_add
-    calculator = Calculator.new
-    actual = calculator.add(2,2)
+    actual = @calculator.add(2,2)
     assert_equal(4, actual)
   end
 
   def test_can_subtract
-    calculator = Calculator.new
-    actual = calculator.subtract(2,2)
+    actual = @calculator.subtract(2,2)
     assert_equal(0, actual)
   end
 
   def test_can_multiply
-    calculator = Calculator.new
-    actual = calculator.multiply(3,2)
+    actual = @calculator.multiply(3,2)
     assert_equal(6, actual)
   end
 end
