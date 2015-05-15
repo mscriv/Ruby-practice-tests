9a412289ebd1581c10e0f7025464e802f5d8610b
Michael Scriven <bigscriv03@gmail.com>
2015-05-15 07:00:30 -0400

    Added Subtraction test

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -1,5 +1,16 @@
 require 'test/unit'
+require 'app/calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  def test_can_add
+    calculator = Calculator.new
+    actual = calculator.add(2,2)
+    assert_equal(4, actual)
+  end
 
+  def test_can_subtract
+    calculator = Calculator.new
+    actual = calculator.subtract(2,2)
+    assert_equal(0, actual)
+  end
 end
